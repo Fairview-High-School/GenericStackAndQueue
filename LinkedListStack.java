e72f8a44c024ad6592867d224cfc72354035afcb
Tim Gesell <timothy.gesell@bvsd.org>
2023-11-03 12:49:42 -0600

    updated some comments

--- LinkedListStack.java
+++ LinkedListStack.java
@@ -1,6 +1,10 @@
 import java.util.LinkedList;
 
-//FIFO Stack, which can store Elements of type E (Where E is any Object type) 
+//FIFO Stack, which can store Elements of type E (Where E is any Object type)
+//NOTE - technically java's implementation of LinkedList is a DoublyLinked List.
+//However, for the purpose of this assignment, lets assume it is a singly linked list
+// (because we will implement stack using a singly-linked Node structure later on in the Unit)
+
 public class LinkedListStack<E> implements Stack<E>
 {
     private LinkedList<E> list;
--- Queue.java
+++ Queue.java
@@ -1,6 +1,7 @@
-//From BluePelicanJava Lesson 52
 public interface Queue<E>
 {
+    //"front" and "back" should be chosen so that each is the most efficient location to perform
+    //that operation for that data structure.
     boolean isEmpty( ); //returns true if nothing in the queue
     void enqueue(E obj ); //places object obj at the back of the queue
     E dequeue( ); //removes and returns object at front of the list
